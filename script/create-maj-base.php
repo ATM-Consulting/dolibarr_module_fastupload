24675ab123cb6d23846338cf8fdbe213d3e490ac
FAVRE Pierre-Henry <phf@atm-consulting.fr>
2017-12-08 10:53:36 +0100

    Update create-maj-base.php

--- script/create-maj-base.php
+++ script/create-maj-base.php
@@ -2,14 +2,14 @@
 /*
  * Script créant et vérifiant que les champs requis s'ajoutent bien
  */
-
+/*
 if(!defined('INC_FROM_DOLIBARR')) {
 	define('INC_FROM_CRON_SCRIPT', true);
 
 	require('../config.php');
 
 }
-
+*/
 
 /* uncomment
 
